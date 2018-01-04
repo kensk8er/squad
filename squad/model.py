b18c4361327c657b4f9c22280a81baa84eca01f1
kensk8er <kensk8er1017@gmail.com>
2018-01-04 17:55:00 +0000

    Implemented the model in `MACHINE COMPREHENSION USING MATCH-LSTM AND ANSWER POINTER`.

--- squad/model.py
+++ squad/model.py
@@ -8,7 +8,6 @@ from random import shuffle
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.layers import xavier_initializer
 
 __author__ = 'Kensuke Muraki'
 
@@ -238,7 +237,7 @@ class QAModel(object):
             contexts, _ = tf.nn.bidirectional_dynamic_rnn(
                 fw_context_cell, bw_context_cell, contexts, sequence_length=self.context_lens,
                 dtype=tf.float32, scope='context/bidirectional_rnn')
-            contexts = tf.concat(contexts, axis=2)
+            contexts = tf.concat(contexts, axis=-1)
 
             # preprocess questions
             fw_question_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.params.state_size)
@@ -246,11 +245,11 @@ class QAModel(object):
             questions, _ = tf.nn.bidirectional_dynamic_rnn(
                 fw_question_cell, bw_question_cell, questions, sequence_length=self.question_lens,
                 dtype=tf.float32, scope='question/bidirectional_rnn')
-            questions = tf.concat(questions, axis=2)
+            questions = tf.concat(questions, axis=-1)
 
         with tf.variable_scope('match-lstm'):
             # attention mechanism
-            attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+            attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
                 num_units=self.params.state_size, memory=questions,
                 memory_sequence_length=self.question_lens)
 
@@ -258,56 +257,42 @@ class QAModel(object):
             fw_decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.params.state_size)
             fw_decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
                 cell=fw_decoder_cell, attention_mechanism=attention_mechanism,
-                attention_layer_size=self.params.state_size)
+                output_attention=False)
 
             # backward decoder
             bw_decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.params.state_size)
             bw_decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
                 cell=bw_decoder_cell, attention_mechanism=attention_mechanism,
-                attention_layer_size=self.params.state_size)
+                output_attention=False)
 
             # decoding with attention
             contexts, _ = tf.nn.bidirectional_dynamic_rnn(
                 fw_decoder_cell, bw_decoder_cell, contexts, sequence_length=self.context_lens,
                 dtype=tf.float32, scope='bidirectional_rnn')
 
-            contexts = tf.concat(contexts, axis=2)
+            contexts = tf.concat(contexts, axis=-1)
 
         with tf.variable_scope('answer-pointer'):
-            # compute shapes
-            context_shape = tf.shape(contexts)
-            batch_size = context_shape[0]
-            max_context_len = context_shape[1]
+            batch_size = tf.shape(contexts)[0]
+            answer_attention = tf.contrib.seq2seq.BahdanauAttention(
+                num_units=self.params.state_size, memory=contexts,
+                memory_sequence_length=self.context_lens)
+            answer_decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.params.state_size)
+            cell_input_fn = lambda input_, attention: attention  # ignore the inputs
+            answer_decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
+                cell=answer_decoder_cell, attention_mechanism=answer_attention,
+                cell_input_fn=cell_input_fn, alignment_history=True)
 
-            # for answer start pointer
-            w_start = tf.get_variable('W_start', shape=(self.params.state_size * 2, 1),
-                                      initializer=xavier_initializer(), dtype=tf.float32)
-            b_start = tf.get_variable('b_start', shape=(1,), initializer=tf.zeros_initializer(),
-                                      dtype=tf.float32)
-            start_logits = tf.reshape(
-                tf.matmul(tf.reshape(contexts, (batch_size * max_context_len, -1)),
-                          w_start) + b_start, (batch_size, max_context_len, 1))
-            start_logits = tf.squeeze(start_logits, axis=2)
-
-            # for answer end pointer
-            w_end = tf.get_variable('W_end', shape=(self.params.state_size * 2, 1),
-                                    initializer=xavier_initializer(), dtype=tf.float32)
-            b_end = tf.get_variable('b_end', shape=(1,), initializer=tf.zeros_initializer(),
-                                    dtype=tf.float32)
-            end_logits = tf.reshape(
-                tf.matmul(tf.reshape(contexts, (batch_size * max_context_len, -1)), w_end) + b_end,
-                (batch_size, max_context_len, -1))
-            end_logits = tf.squeeze(end_logits, axis=2)
+            # we ignore the inputs
+            dummy_inputs = [tf.zeros(shape=(batch_size,)), tf.zeros(shape=(batch_size,))]
+            _, states = tf.nn.static_rnn(answer_decoder_cell, dummy_inputs, dtype=tf.float32)
+            self.probabilities = states.alignment_history.stack()
 
         with tf.variable_scope('loss'):
-            # add large values to valid logits so that softmax are near accurate even with paddings
-            start_logits = start_logits + tf.multiply(
-                tf.sequence_mask(self.context_lens, dtype=tf.float32), self.params.large_value)
-            end_logits = end_logits + tf.multiply(
-                tf.sequence_mask(self.context_lens, dtype=tf.float32), self.params.large_value)
-            start_loss = tf.losses.sparse_softmax_cross_entropy(self.answer_start_ids, start_logits)
-            end_loss = tf.losses.sparse_softmax_cross_entropy(self.answer_end_ids, end_logits)
-            self.loss = start_loss + end_loss
+            epsilon = 1e-8
+            logits = tf.transpose(tf.log(self.probabilities + epsilon), (1, 0, 2))
+            labels = tf.stack((self.answer_start_ids, self.answer_end_ids), axis=1)
+            self.loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
 
         with tf.variable_scope('optimizer'):
             optimizer = tf.train.AdamOptimizer(learning_rate=self.params.learning_rate)
@@ -317,9 +302,9 @@ class QAModel(object):
             self.grad_norm = tf.global_norm(grads)
 
         with tf.variable_scope('prediction'):
-            self.start_probabilities = tf.nn.softmax(start_logits)
+            self.start_probabilities = tf.gather(self.probabilities, 0)
+            self.end_probabilities = tf.gather(self.probabilities, 1)
             self.start_predictions = tf.cast(tf.argmax(self.start_probabilities, axis=1), tf.int32)
-            self.end_probabilities = tf.nn.softmax(end_logits)
             self.end_predictions = tf.cast(tf.argmax(self.end_probabilities, axis=1), tf.int32)
             self.start_accuracy = tf.reduce_mean(
                 tf.cast(tf.equal(self.start_predictions, self.answer_start_ids), dtype=tf.float32))
